2c05f34c67ee4e4feb701e62554366cb53816794
Richard Hurt <rnhurt@gmail.com>
2018-12-18 10:48:32 -0500

    Fix SNS email processing

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require 'time'
 module LambdaFunctions
   class Handler
     def self.process(event:, context:)
-      puts event
+      # puts event
       data = event['Records'].first
       notifier = Slack::Notifier.new ENV["SLACK_WEBHOOK_URL"]
 
@@ -18,30 +18,57 @@ module LambdaFunctions
       when 'aws:sns'
         case data['Sns']['Type']
         when 'Notification'
-          subject = data['Sns']['Subject']
           message = JSON.parse(data['Sns']['Message'])
-          region = data['EventSubscriptionArn'].split(':')[3]
-          
-          trigger = "#{message['Trigger']['Statistic']} #{message['MetricName']} "
-          trigger += "#{message['Trigger']['ComparisonOperator']} "
-          trigger += "#{message['Trigger']['Threshold']} for "
-          trigger += "#{message['Trigger']['EvaluationPeriods']} period(s) of "
-          trigger += "#{message['Trigger']['Period']} seconds."
+          if message['notificationType']
+            # This is an SES mail delivery notification
+            subject = "SES Message Bounce (#{message['bounce']['bounceType']} - #{message['bounce']['bounceSubType']})"
+            from = message['mail']['source']
 
-          fields = [
-            { title: 'Alarm Name', value: message['AlarmName'], short: true },
-            { title: 'Alarm Description', value: message['AlarmReason'], short: false },
-            { title: 'Trigger', value: trigger, short: false },
-            { title: 'Old State', value: message['OldStateValue'], short: true },
-            { title: 'Current State', value: message['NewStateValue'], short: true },
-            { title: 'Link to alarm', 
-              value: "https://console.aws.amazon.com/cloudwatch/home?region=#{region}#alarm:alarmFilter=ANY;name=#{URI.encode(message['AlarmName'])}",
-              short: false
-            }
-          ]
-          timestamp = Time.parse(message['StateChangeTime']).to_i rescue 1
-          color = message['NewStateValue'] == "ALARM" ? 'danger' : 'good'
+            # Get better headers, if possible
+            unless message['mail']['headersTruncated']
+              subject = message['mail']['commonHeaders']['subject']
+              from = message['mail']['commonHeaders']['from'][0]
+            end
 
+            recipients = ""
+            message['bounce']['bouncedRecipients'].each do |recipient|
+              recipients += "* #{recipient['emailAddress']} (#{recipient['action']}): #{recipient['diagnosticCode']}\n"
+            end
+
+            fields = [
+              { title: 'From', value: from, short: false },
+              { title: 'Recipients', value: recipients, short: false }
+            ]
+            timestamp = Time.parse(message['StateChangeTime']).to_i rescue 1
+            color = message['bounce']['bounceType'] == 'Transient' ? 'warning' : 'danger'
+
+          elsif message['AlarmName']
+            # This is a CloudWatch ALARM notification
+            subject = data['Sns']['Subject']
+            region = data['EventSubscriptionArn'].split(':')[3]
+
+            trigger = "#{message['Trigger']['Statistic']} #{message['MetricName']} "
+            trigger += "#{message['Trigger']['ComparisonOperator']} "
+            trigger += "#{message['Trigger']['Threshold']} for "
+            trigger += "#{message['Trigger']['EvaluationPeriods']} period(s) of "
+            trigger += "#{message['Trigger']['Period']} seconds."
+
+            fields = [
+              { title: 'Alarm Name', value: message['AlarmName'], short: true },
+              { title: 'Alarm Description', value: message['AlarmReason'], short: false },
+              { title: 'Trigger', value: trigger, short: false },
+              { title: 'Old State', value: message['OldStateValue'], short: true },
+              { title: 'Current State', value: message['NewStateValue'], short: true },
+              { title: 'Link to alarm',
+                value: "https://console.aws.amazon.com/cloudwatch/home?region=#{region}#alarm:alarmFilter=ANY;name=#{URI.encode(message['AlarmName'])}",
+                short: false
+              }
+            ]
+            timestamp = Time.parse(message['StateChangeTime']).to_i rescue 1
+            color = message['NewStateValue'] == "ALARM" ? 'danger' : 'good'
+          else
+            subject = "Unknown SNS message"
+          end
         else
           subject = "Unknown SNS message Type"
         end
@@ -60,7 +87,7 @@ module LambdaFunctions
         icon_emoji: ":aws:",
         ts: timestamp
       }
-      
+
       # Post the message to Slack
       notifier.post attachments: [attachment]
 
